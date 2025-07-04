54afea602e356a7f0ad789647686c7e0680d6814
Hi, I'm K Ramesh <115610243+KRameshr@users.noreply.github.com>
2025-07-04 07:38:13 +0530

    Add files via upload

--- Day3/DemoPromise/index.js
+++ Day3/DemoPromise/index.js
@@ -1,90 +1,19 @@
-// let FavNumber=[21,36,96,98,58,54,21,23,42,15,685,12,58,1254,69,5]
+//  express is fram works
 
-// let promise = new Promise(function(resolve,reject){
-//     setTimeout(() => {
-//         if(true){resolve(FavNumber)}
-//         else{reject("rejects")}
-//     }, 4000);
-// })
-// // permise are resolved
 
-// promise
-// .then(function(res){
-//     console.log("Resolve",res);
-//     return res.filter(function(val){
-//         return val >50
-//     })
-// })
-// .then(function(res){
-//     console.log("Resolve",res)
-//     return res.reduce(function(int ,val){
-//         return int+val;
-//     })
-// })
-// .then(function(res){
-//     console.log("Resolve",res)
-// })
-// .catch(function(err){
-//     console.log("Rejected",err)
-// })
-// .finally(function(){
-//     console.log("Promise are now setteled")
-// })
+const express = require("express");
+const app = express();
 
-var promise1 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    if (true) {
-      resolve("Promise ONe Is resolved");
-    } else {
-      reject("Peomise ONe is Rejected");
-    }
-  }, 6000);
-});
-var promise2 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    if (true) {
-      resolve("Promise Two Is resolved");
-    } else {
-      reject("Peomise Two is Rejected");
-    }
-  }, 9000);
-});
-var promise3 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    if (true) {
-      resolve("Promise Three Is resolved");
-    } else {
-      reject("Peomise Three is Rejected");
-    }
-  }, 7000);
-});
-var promise4 = function () {
-  return new Promise(function (resolve, reject) {
-    setTimeout(() => {
-      if (true) {
-        resolve("Promise Foure Is resolved");
-      } else {
-        reject("Peomise Three is Rejected");
-      }
-    }, 4000);
-  });
-};
-// promise1.then().catch().finally()
-// promise2.then().catch().finally()
-// promise3.then().catch().finally()
+app.get('/',(req,res)=>{
+    // res.writeHead(200,{"Content-Type":"text/plain"});
+    // res.write("wellcom")
+    // res.end()
 
-// Promise.all([promise1,promise2,promise3,promise4()])
-// .then(res => console.log(res))
-// .catch(err => console.log(err))
-// .finally("all setteled");
+    res.send({"message":"wellcome to your life"});
+    res.json({"message":"wellcome to your life"});
+})
 
-//race will consider resolve and reject
-Promise.race([promise1, promise2, promise3, promise4()])
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err))
-  .finally("all setteled");
-
-Promise.allSettled([promise1, promise2, promise3, promise4()])
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err))
-  .finally("all setteled");
+app.listen(9009,"localhost",(err)=>{
+if(err) console.log(err)
+    else console.log("server now lives at 9009")
+})
